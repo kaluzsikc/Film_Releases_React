5e34cf9ad8c1fb519fbe7be7773fb51b689fac7c
kaluzsikc <kaluzsikc@gmail.com>
2018-10-29 16:18:30 +0000

    Created FilmBox class and removed the logo

---
+++ src/contaiers/FilmBox.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+class FilmBox extends Component {
+
+
+    render(){
+        return(
+            <h1>hello</h1>
+        )
+    }
+
+}
+
+export default FilmBox;
